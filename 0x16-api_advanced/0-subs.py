a3e57acfa99ce7e323cbc803b59ae7f8938b05ac
elisa2105 <aikomi1726@gmail.com>
2020-05-08 02:29:41 +0000

    fixed session to avoid 429

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """ subscribers list"""
-    # url for get subscribers according to the API
     url = "https://www.reddit.com/r/subreddit/about.json"
-    # User-Agent to avoid too many requests 429
     headers = {'user-Agent': 'Python3'}
-    # call http and add headers
-    request = requests.get(url, params=headers)
-    print(request)
-    # get the subscribers list
+    stre = requests.session()
+    stre.headers = headers
+    request = stre.get(url)
     if request.status_code == 200:
         return request.json().get('data').get('subscribers')
     else:
