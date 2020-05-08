cfa4baef47bd11add4f8cfc34fc649deee04615d
elisa2105 <aikomi1726@gmail.com>
2020-05-08 02:44:50 +0000

    added close session format and no str

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,17 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ subscribers list"""
-    url = "https://www.reddit.com/r/subreddit/about.json"
+    if type(subreddit) is not str:
+        return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'user-Agent': 'Python3'}
     stre = requests.session()
     stre.headers = headers
     request = stre.get(url)
     if request.status_code == 200:
+        stre.close()
         return request.json().get('data').get('subscribers')
     else:
+        stre.close()
         return 0
+    
